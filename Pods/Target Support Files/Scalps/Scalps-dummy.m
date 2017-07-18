e5c8d4f0d5d2d8e7d73bc6e2a91594092ae0b2dd
Wen <jmd@wl2s-237-13.unil.ch>
2017-07-18 11:19:19 +0200

    correct error

--- Pods/Target Support Files/Scalps/Scalps-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Scalps : NSObject
-@end
-@implementation PodsDummy_Scalps
-@end
--- Pods/Target Support Files/ScalpsSDK/ScalpsSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ScalpsSDK : NSObject
-@end
-@implementation PodsDummy_ScalpsSDK
-@end
